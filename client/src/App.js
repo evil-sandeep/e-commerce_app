e496b26bedd717c2c53277baeae082c27d0c339a
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-10-09 16:32:18 +0530

    Adding header and Footer section

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-
+// Desc: Main App component
 import React from 'react';
+import {Container} from 'react-bootstrap'; 
+import Header from './component/Header';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    < >
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to sandeep</h1>
+        </Container>
+          
+        <h1>App</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/component/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copy Right &copy; sandeep
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
