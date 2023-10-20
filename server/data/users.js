a6bc87622b0e3749880c23d6d32727cacb5718f8
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-10-20 12:39:51 +0530

    sample data for admin and user for login and authentication

---
+++ server/data/users.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcryptjs';
+// Data for users
+
+const users=[
+    {
+        name:'Admin user',
+        email:'chandansandeep@gmail.com',
+        password:bcrypt.hashSync('123456',10),
+        isAdmin:true,
+    },
+    {
+        name:'John Doe',
+        email:'john@gmail.com',
+        password:bcrypt.hashSync('123456',10),
+        
+    },
+    {
+        name:'Jane Doe',
+        email:'jane@gmail.com',
+        password:bcrypt.hashSync('123456',10),
+    } 
+
+]
