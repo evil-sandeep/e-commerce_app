57146424adb0a89f18934d0a3c53d404ab66a10a
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-10-22 19:49:08 +0530

    Adding Loader/Shimmer or message Components

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -3,33 +3,36 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../component/Product'
 import { listProducts } from '../actions/productActions'
+import Loader from '../component/Loader'
+import Message from '../component/Message'
 
 
 
 const HomeScreen = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch();                         //access the redux store
     const productList = useSelector(state => state.productList)
-    const{ loading,error,products}=productList;
+    const { loading, error, products } = productList;
 
-    useEffect(() => {
+    useEffect(() => {                                //Fetch Products with Redux Action
         dispatch(listProducts())
     }, [dispatch])
 
-   
 
+    //Render Products Based on Redux State
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? <h2>Loading...Wait a Minute </h2> : error ? <h3>{error}</h3> :  <Row>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
+            ) : <Row>
                 {products.map((product) => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
                         <Product product={product} />
                     </Col>
                 ))}
-            </Row> }
-
-           
-
+            </Row>}
         </>
     )
 }
