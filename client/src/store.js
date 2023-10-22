a89014442def99cec89a843c939f55428b16f588
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-10-22 16:08:59 +0530

    product list reducer and action

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,25 @@
 // Desc: Redux store for the application
-import { createStore, combineReducers, applyMiddleware } 
-from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { productListReducer } from './reducers/productReducer';
 
 // import {productListReducer,productDetailsReducer} from './reducers/productReducers';
 
-const reducer = combineReducers({}) //replace this empty object with actual reducer functions.
+
+const reducer = combineReducers({
+    productList: productListReducer,
+    // productDetails:productDetailsReducer,
+})
+
+
 const initialState = {};// provide default values for  application's initial state if needed.
 const middleware = [thunk];//allows  to write asynchronous actions in Redux.
 
-const store=createStore(
+const store = createStore(
     reducer,
     initialState,
     composeWithDevTools(applyMiddleware(...middleware))
 );
- 
+
 export default store;
