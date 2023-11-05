baa9fdbbf89f31f262c61912a87a2a96ac87b105
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-11-05 12:23:30 +0530

    Custom Authentication  Middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,14 +1,9 @@
 import express from 'express'; // create API routes.
 const router = express.Router();//used to define and organize routes.
-// import {
-//     authUser,
-// } from '../controllers/userController';
-
-import { authUser } from '../controllers/userController.js';
-
-
-
+import { authUser,getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
+router.route('/profile').get(protect,getUserProfile);
 
 export default router;
