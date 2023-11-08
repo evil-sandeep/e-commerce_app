205fa69f7c3bd899f3d3fac276758d3271591265
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-11-08 12:50:43 +0530

    Update Profile End Point

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'; // create API routes.
 const router = express.Router();//used to define and organize routes.
-import { authUser,getUserProfile,registerUser } from '../controllers/userController.js';
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
-router.route('/profile').get(protect,getUserProfile);
+router.route('/profile')
+      .get(protect, getUserProfile)
+      .put(protect, updateUserProfile);
 router.route('/').post(registerUser)
 
+
 export default router;
- 
