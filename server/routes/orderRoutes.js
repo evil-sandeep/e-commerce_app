15a4d99889ed37639422b5e3d64f0689158e0f63
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2024-03-09 10:53:49 +0530

    Update to paid endpoint

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'; // create API routes.
 const router = express.Router();//used to define and organize routes.
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 
 export default router;
